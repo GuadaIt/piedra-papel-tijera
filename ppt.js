ff3fb541c72fc10a1290b178b377d4c8ea0615b8
GuadaIt <guada.iturralde@gmail.com>
2019-11-11 21:52:13 -0300

    agregué for, 2 rondas

--- ppt.js
+++ ppt.js
@@ -1,34 +1,57 @@
 /////////////////PIEDRA PAPEL O TIJERA///////////////////////
-let valorIngresado1 = prompt("¿Piedra, papel o tijera?");
-let valorIngresado2
+let valorIngresado1; 
+let valorIngresado2;
+let partidasGanadas = 2;
+let puntajeJugador1 = 0;
+let puntajeJugador2 = 0;
 
-if (valorIngresado1 === "piedra" || valorIngresado1 === "papel" || valorIngresado1 === "tijera") {
-  valorIngresado2 = prompt("¿Piedra, papel o tijera?")
-} if (valorIngresado1 === valorIngresado2) {
-    alert(`Empate`)
+for (i = 0; i < 2; i++) {
+  valorIngresado1 = prompt("¿Piedra, papel o tijera?")
+  if (valorIngresado1 === "piedra" || valorIngresado1 === "papel" || valorIngresado1 === "tijera") {
+    valorIngresado2 = prompt("¿Piedra, papel o tijera?")
+  } if (valorIngresado1 === valorIngresado2) {
+      alert(`Empate`)
   }
-  else if (valorIngresado1 === "piedra" && valorIngresado2 === "papel") {
-    alert(`Ganador: Jugador 2`)
-  }
-  else if (valorIngresado1 === "piedra" && valorIngresado2 === "tijera") {
-    alert(`Ganador: Jugador 1`)
-  }
-  else if (valorIngresado1 === "papel" && valorIngresado2 === "tijera") {
-    alert(`Ganador: Jugador 2`)
-  }
-  else if (valorIngresado1 === "papel" && valorIngresado2 === "piedra") {
-    alert(`Ganador: Jugador 1`)
-  }
-  else if (valorIngresado1 === "tijera" && valorIngresado2 === "piedra") {
-    alert(`Ganador: Jugador 2`)
-  }
-  else if (valorIngresado1 === "tijera" && valorIngresado2 === "papel") {
-    alert(`Ganador: Jugador 1`)
-  }        
-else {
-    alert(`No podes jugar`)
+    else if (valorIngresado1 === "piedra" && valorIngresado2 === "papel") {
+      puntajeJugador2++
+      alert(`Ganador: Jugador 2`)
+    }
+    else if (valorIngresado1 === "piedra" && valorIngresado2 === "tijera") {
+      puntajeJugador1++
+      alert(`Ganador: Jugador 1`)
+    }
+    else if (valorIngresado1 === "papel" && valorIngresado2 === "tijera") {
+      puntajeJugador2++
+      alert(`Ganador: Jugador 2`)
+    }
+    else if (valorIngresado1 === "papel" && valorIngresado2 === "piedra") {
+      puntajeJugador1++
+      alert(`Ganador: Jugador 1`)
+    }
+    else if (valorIngresado1 === "tijera" && valorIngresado2 === "piedra") {
+      puntajeJugador2++
+      alert(`Ganador: Jugador 2`)
+    }
+    else if (valorIngresado1 === "tijera" && valorIngresado2 === "papel") {
+      puntajeJugador1++
+      alert(`Ganador: Jugador 1`)
+    }
+    else if (puntajeJugador1 < puntajeJugador2) {
+      alert(`Ganador: Jugador 2`)
+    }
+    else if (puntajeJugador1 > puntajeJugador2) {
+      alert(`Ganador: JUgador 1`)
+    }         
+  else {
+      alert(`No podes jugar`)
   };
+}
 
+if (puntajeJugador1 < puntajeJugador2) {
+  alert(`El ganador es el jugador 2`);
+} else {
+  alert(`El ganador es el jugador 1`);
+  }
 
 /*Utilizando todo lo visto hasta ahora, hagamos un juego de piedra, papel o tijera
 Dos usuarios deben ingresar su opcion elegida mediate dos prompts.
